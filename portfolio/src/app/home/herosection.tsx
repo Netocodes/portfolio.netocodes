343a1d922da10f688dc76374fa42c59189b36436
Netochukwu Codes <161637474+Netocodes@users.noreply.github.com>
2025-04-05 14:52:04 +0100

    Enhance HeroSection: Update RotatingText styles and increase rotation interval

--- portfolio/src/app/home/herosection.tsx
+++ portfolio/src/app/home/herosection.tsx
@@ -8,13 +8,14 @@ const HeroSection = () => {
   return (
     <div className="h-[500px] flex items-center justify-center bg-blue-300 dark:bg-blue-950">
       <div className="flex flex-col items-center justify-center gap-y-5">
+
      
 
 <div className="flex flex-col lg:flex-row gap-4 items-center">
   <h1>Here at Netocodes We build</h1>
 <RotatingText
   texts={['E-Cormerce Websites', 'Buisness Websites', 'Logistics Websites', 'Everything Website Related']}
-  mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+  mainClassName="px-2 sm:px-2 text-3xl md:px-3 bg-cyan-300 dark:bg-blue-800 dark:text-white  text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
   staggerFrom={"last"}
   initial={{ y: "100%" }}
   animate={{ y: 0 }}
@@ -22,7 +23,7 @@ const HeroSection = () => {
   staggerDuration={0.025}
   splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
   transition={{ type: "spring", damping: 30, stiffness: 400 }}
-  rotationInterval={2000}
+  rotationInterval={3000}
 />
 </div>
 
