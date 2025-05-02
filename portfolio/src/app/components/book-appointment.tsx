fad4910f6ae7eae7118b8250176857194430413b
Netochukwu Codes <161637474+Netocodes@users.noreply.github.com>
2025-05-02 13:04:55 +0100

    Update book-appointment.tsx

--- portfolio/src/app/components/book-appointment.tsx
+++ portfolio/src/app/components/book-appointment.tsx
@@ -32,7 +32,7 @@ export default function Appointment() {
     <button className="  fixed rounded-full z-50 bottom-5 right-5 aurora bg-[#1013d7]  text-white dark:text-black dark:bg-cyan-300 hover:shadow-[#333333]/20 focus:shadow-[#333333]/20 active:shadow-[#333333]/10"   >
       <a className="flex items-center py-2 px-3 gap-x-1" href="https://cal.com/netochukwu-codes/" target="_parent" rel="noopener noreferrer">
       <Headset size={32} className="" />
-      Chat with me
+      Chat with me!
       </a>
   </button>
   );
